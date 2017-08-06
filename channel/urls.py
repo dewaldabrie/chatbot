df50a380a100aaa90447a74280c5c0a6a50ea762
Dewald Abrie <dewaldabrie@gmail.com>
2017-08-07 00:24:27 +1000

    Create channel app to allow for interaction between client and bot.
    Bot is a simple scripted questionaire bot.
    Converstations and users are tracked with randomly generated tokens.
    View templates are yet to be created.

--- channel/urls.py
+++ channel/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from channel.views import *
 
 urlpatterns = [
     # Examples:
+    url(r'^$', talk_to_bot, name='talk_to_bot'),
 ]
